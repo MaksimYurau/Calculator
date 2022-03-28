c4a35ee8600c4e4f0b06f4608bfefb3880947b21
MaksimYurau <2004softwaredeveloper@gmail.com>
2022-03-28 18:16:09 +0300

    Updated project. Added CalculatorModel.kt. Updated MainActivity.kt. Updated gradle.xml. Updated misc.xml.

---
+++ app/src/main/java/com/maksimyurau/android/calculator/CalculatorModel.kt
@@ -0,0 +1,104 @@
+package com.maksimyurau.android.calculator
+
+class CalculatorModel {
+    private var firstArg = 0
+    private var secondArg = 0
+    private val inputStr = StringBuilder()
+    private var actionSelected = 0
+    private var state: State
+
+    private enum class State {
+        firstArgInput, operationSelected, secondArgInput, resultShow
+    }
+
+    fun onNumPressed(buttonId: Int) {
+        if (state == State.resultShow) {
+            state = State.firstArgInput
+            inputStr.setLength(0)
+        }
+        if (state == State.operationSelected) {
+            state = State.secondArgInput
+            inputStr.setLength(0)
+        }
+        if (inputStr.length < 9) {
+            when (buttonId) {
+                R.id.zero -> if (inputStr.isNotEmpty()) {
+                    inputStr.append("0")
+                }
+                R.id.one -> inputStr.append("1")
+                R.id.two -> inputStr.append("2")
+                R.id.three -> inputStr.append("3")
+                R.id.four -> inputStr.append("4")
+                R.id.five -> inputStr.append("5")
+                R.id.six -> inputStr.append("6")
+                R.id.seven -> inputStr.append("7")
+                R.id.eight -> inputStr.append("8")
+                R.id.nine -> inputStr.append("9")
+            }
+        }
+    }
+
+    fun onActionPressed(actionId: Int) {
+        if (actionId == R.id.equals && state == State.secondArgInput && inputStr.isNotEmpty()) {
+            secondArg = inputStr.toString().toInt()
+            state = State.resultShow
+            inputStr.setLength(0)
+            when (actionSelected) {
+                R.id.plus -> inputStr.append(firstArg + secondArg)
+                R.id.minus -> inputStr.append(firstArg - secondArg)
+                R.id.multiply -> inputStr.append(firstArg * secondArg)
+                R.id.division -> inputStr.append(firstArg / secondArg)
+            }
+        } else if (inputStr.isNotEmpty() && state == State.firstArgInput && actionId != R.id.equals) {
+            firstArg = inputStr.toString().toInt()
+            state = State.operationSelected
+            actionSelected = actionId
+        }
+    }
+
+    val text: String
+        get() {
+            val str = StringBuilder()
+            return when (state) {
+                State.operationSelected -> str.append(
+                    firstArg
+                ).append(' ')
+                    .append(operationChar)
+                    .toString()
+                State.secondArgInput -> str.append(
+                    firstArg
+                ).append(' ')
+                    .append(operationChar)
+                    .append(' ')
+                    .append(inputStr)
+                    .toString()
+                State.resultShow -> str.append(firstArg)
+                    .append(' ')
+                    .append(operationChar)
+                    .append(' ')
+                    .append(secondArg)
+                    .append(" = ")
+                    .append(inputStr.toString())
+                    .toString()
+                else -> inputStr.toString()
+            }
+        }
+
+    private val operationChar: Char
+        get() = when (actionSelected) {
+            R.id.plus -> '+'
+            R.id.minus -> '-'
+            R.id.multiply -> '*'
+            R.id.division -> '/'
+            else -> '/'
+        }
+
+    fun reset() {
+        state = State.firstArgInput
+        inputStr.setLength(0)
+    }
+
+    init {
+        state = State.firstArgInput
+    }
+}
--- app/src/main/java/com/maksimyurau/android/calculator/MainActivity.kt
+++ app/src/main/java/com/maksimyurau/android/calculator/MainActivity.kt
@@ -1,11 +1,65 @@
 package com.maksimyurau.android.calculator
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+
+    private var calculator: CalculatorModel? = null
+    private lateinit var text: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val numberIds = intArrayOf(
+            R.id.zero,
+            R.id.one,
+            R.id.two,
+            R.id.three,
+            R.id.four,
+            R.id.five,
+            R.id.six,
+            R.id.seven,
+            R.id.eight,
+            R.id.nine
+        )
+
+        val actionsIds = intArrayOf(
+            R.id.plus,
+            R.id.minus,
+            R.id.multiply,
+            R.id.division,
+            R.id.equals
+        )
+
+        text = findViewById(R.id.text)
+
+        calculator = CalculatorModel()
+
+        val numberButtonClickListener = View.OnClickListener { view ->
+            calculator!!.onNumPressed(view.id)
+            text.text = calculator!!.text
+        }
+
+        val actionButtonOnclickListener = View.OnClickListener { view ->
+            calculator!!.onActionPressed(view.id)
+            text.text = calculator!!.text
+        }
+
+        for (numberId in numberIds) {
+            findViewById<View>(numberId).setOnClickListener(numberButtonClickListener)
+        }
+
+        for (actionsId in actionsIds) {
+            findViewById<View>(actionsId).setOnClickListener(actionButtonOnclickListener)
+        }
+
+        findViewById<View>(R.id.reset).setOnClickListener {
+            calculator!!.reset()
+            text.text = calculator!!.text
+        }
     }
 }
